e529d14346610c0dd8cbff2726249dc00b5d0e80
user08lasya <lasyapriya0807@gmail.com>
2024-11-18 20:47:58 +0530

    Operation on file-1

---
+++ activity3.py
@@ -0,0 +1,8 @@
+file1 = open('Codingal.txt','r')
+file2 = open('2.txt','w')
+for line in file1.readlines():
+    if not (line.startswith('Coding')):
+        print(line)
+        file2.write(line)
+file2.close()
+file1.close()
